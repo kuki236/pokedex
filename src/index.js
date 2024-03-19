240b8e6af2711a5fed14847f9ae696bedc93e92b
Cuuuckiii <fanreaccioneselitecraft@gmail.com>
2024-03-18 23:48:24 -0500

    agregar cuerpoModal

--- src/index.js
+++ src/index.js
@@ -35,11 +35,9 @@ function mostrarPagina(numeroPagina = 1) {
         });
         asignarModalPokemon();
         mostrarModalPokemon();
-        // Resolvemos la promesa después de agregar los Pokémon a la lista
         resolve();
       })
       .catch(error => {
-        // En caso de error, rechazamos la promesa
         reject(error);
       });
   });
@@ -63,41 +61,55 @@ function crearModal(nombrePokemon){
     fetch(urlDatosPokemon)
       .then(respuesta => respuesta.json())
       .then(respuestaJson =>{
-
         let $imagenPokemonModal = document.querySelector('.imagenPokemonModal')
+        limpiarModal()
         let urlImagenPokemonModal = respuestaJson.sprites.front_default
         $imagenPokemonModal.setAttribute('src',urlImagenPokemonModal)
-
         let $modalTipo = document.querySelector('.tiposPokemon')
+        let $tipoSaludo = document.createElement('p')
+        $tipoSaludo.classList.add('datosModal')
+        $tipoSaludo.textContent = 'Tipos: '
+        $modalTipo.appendChild($tipoSaludo)
         respuestaJson.types.forEach(tipo =>{
-          let tipoPokemon  = ''
-          tipoPokemon = tipo.type.name
+          let tipoPokemon = tipo.type.name
           let $tipoModal = document.createElement('p')
-          console.log('tipoPokemon')
           $tipoModal.textContent = tipoPokemon
+          $tipoModal.classList.add(tipoPokemon)
+          $tipoModal.classList.add('tipoPokemonEstilo')
           $modalTipo.appendChild($tipoModal)
         })
-
-
         let $pModalHabilidad = document.querySelector('.habilidadesPokemon')
+        let $habilidadSaludo = document.createElement('p')
+        $habilidadSaludo.textContent = 'Habilidades: '
+        $habilidadSaludo.classList.add('datosModal')
+        $pModalHabilidad.appendChild($habilidadSaludo)
         respuestaJson.abilities.forEach(ability =>{
           let habilidad = ability.ability.name
           let $pHabilidad = document.createElement('p')
           $pHabilidad.textContent = habilidad
+          $pHabilidad.classList.add('habilidadModal')
           $pModalHabilidad.appendChild($pHabilidad)
         })
         let peso = respuestaJson.weight
         let $pesoPokemon = document.querySelector('.pesoPokemon')
         $pesoPokemon.textContent = `Peso: ${peso}`
+        $pesoPokemon.classList.add('datosModal')
         let altura = respuestaJson.height
         let $alturaPokemon = document.querySelector('.tallaPokemon')
         $alturaPokemon.textContent = `Altura: ${altura}`
+        $alturaPokemon.classList.add('datosModal')
         resolve();
       })
       .catch(error => {
         reject(error);
       });
   }) 
+}
+function limpiarModal(){
+  document.querySelector('.tiposPokemon').innerHTML = ''
+  document.querySelector('.habilidadesPokemon').innerHTML = ''
+  document.querySelector('.pesoPokemon').innerHTML = ''
+  document.querySelector('.tallaPokemon').innerHTML = ''
 }
 function mostrarModalPokemon(){
   let espacioPokemonModal = document.querySelectorAll('.espacioPokemon')
