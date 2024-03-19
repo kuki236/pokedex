da3a6b44425ffb01b2ea725fb9422f49af7c15c6
Cuuuckiii <fanreaccioneselitecraft@gmail.com>
2024-03-19 18:40:40 -0500

    agregar modulos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { mostrarPagina } from "./interfaz/interfaz.js";
+import { mostrarPagina } from "./interfaz/interfazPagina.js";
 import { crearPaginador} from "./paginador-inicio/paginador-inicio.js";
+import { logicaPaginaAnteriorSiguiente } from "./paginador/paginador.js";
 mostrarPagina();
-
 crearPaginador();
-
+logicaPaginaAnteriorSiguiente()
 
 
 
--- src/paginador/paginador.js
+++ src/paginador/paginador.js
@@ -1,36 +1,50 @@
-let paginasPokemones = document.querySelectorAll('.paginaPokemon')
-let paginaPokemonSiguiente = document.querySelector('.siguiente')
-let paginaPokemonAnterior = document.querySelector('.anterior')
-paginaPokemonAnterior.classList.remove('disabled')
+import { mostrarPagina } from "../interfaz/interfazPagina.js"
+export function logicaPaginaAnteriorSiguiente(){
+    manejarClicPagina()
+    manejarClicAnterior()
+    manejarClicSiguiente()
+}
 
-paginasPokemones.forEach(pagina =>{
-    pagina.addEventListener('click',function(){
-      let namePagina = pagina.getAttribute('name')
-      paginasPokemones.forEach(pagina => {
-        pagina.classList.remove('paginaActual');
-      });
-      pagina.classList.add('paginaActual')
-      mostrarPagina(namePagina)
-    })
-  })
-paginaPokemonAnterior.addEventListener('click',function(){
-  let paginaActual = document.querySelector('.paginaActual')
-  let numeroPaginaActual = paginaActual.getAttribute('name')
-  let numeroPaginaAnterior = Number(numeroPaginaActual)-1
-  let $paginaAnterior = asignarActualPagina(numeroPaginaAnterior)
-  $paginaAnterior.classList.add('paginaActual')
-  mostrarPagina(numeroPaginaAnterior)
-  paginaActual.classList.remove('paginaActual')
-})
-paginaPokemonSiguiente.addEventListener('click',function(){
-  let paginaActual = document.querySelector('.paginaActual')
-  let numeroPaginaActual = paginaActual.getAttribute('name')
-  let numeroPaginaSiguiente = Number(numeroPaginaActual)+1
-  let $paginaSiguiente = asignarActualPagina(numeroPaginaSiguiente)
-  $paginaSiguiente.classList.add('paginaActual')
-  mostrarPagina(numeroPaginaSiguiente)
-  paginaActual.classList.remove('paginaActual')
-})
+
+function manejarClicPagina(){
+    let paginasPokemones = document.querySelectorAll('.paginaPokemon')
+    paginasPokemones.forEach(pagina =>{
+        pagina.addEventListener('click',function(){
+          let namePagina = pagina.getAttribute('name')
+          paginasPokemones.forEach(pagina => {
+            pagina.classList.remove('paginaActual');
+          });
+          pagina.classList.add('paginaActual')
+          mostrarPagina(namePagina)
+        })
+      })
+}
+function manejarClicAnterior(){
+    let paginaPokemonAnterior = document.querySelector('.anterior')
+    paginaPokemonAnterior.classList.remove('disabled')
+    paginaPokemonAnterior.addEventListener('click',function(){
+        let paginaActual = document.querySelector('.paginaActual')
+        let numeroPaginaActual = paginaActual.getAttribute('name')
+        let numeroPaginaAnterior = Number(numeroPaginaActual)-1
+        let $paginaAnterior = asignarActualPagina(numeroPaginaAnterior)
+        $paginaAnterior.classList.add('paginaActual')
+        mostrarPagina(numeroPaginaAnterior)
+        paginaActual.classList.remove('paginaActual')
+      })
+}
+function manejarClicSiguiente(){
+    let paginaPokemonSiguiente = document.querySelector('.siguiente')
+    paginaPokemonSiguiente.addEventListener('click',function(){
+        let paginaActual = document.querySelector('.paginaActual')
+        let numeroPaginaActual = paginaActual.getAttribute('name')
+        let numeroPaginaSiguiente = Number(numeroPaginaActual)+1
+        let $paginaSiguiente = asignarActualPagina(numeroPaginaSiguiente)
+        $paginaSiguiente.classList.add('paginaActual')
+        mostrarPagina(numeroPaginaSiguiente)
+        paginaActual.classList.remove('paginaActual')
+      })
+}
+
 function asignarActualPagina(namePaginaPendiente){
     let paginaEncontrada = null;
     document.querySelectorAll('.paginaPokemon').forEach(pagina =>{
